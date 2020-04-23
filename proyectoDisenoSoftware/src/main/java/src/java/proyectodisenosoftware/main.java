717a77675a2fee8e20ab7639d9d2e98cb030a756
unaipuelles <unai-p@hotmail.com>
2020-04-23 12:38:43 +0200

    Creación de proyecto en Netbeans

---
+++ proyectoDisenoSoftware/src/main/java/src/java/proyectodisenosoftware/main.java
@@ -0,0 +1,15 @@
+package src.java.proyectodisenosoftware;
+
+/**
+ *
+ * @author unaipuelles
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+    }
+}
